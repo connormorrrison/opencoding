f6c82c4b38da38908fb87f1fd2aedbb3e99953bb
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-05-10 06:32:25 -0600

    Adjusted spacing of logo and auth links

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,17 +5,24 @@ export default function Home() {
     <div className="min-h-screen relative bg-white">
       {/* Header */}
       <header>
+        {/* Logo in top-left */}
+        <div className="absolute top-6 left-6">
+          <a href="#" className="px-1 py-1 text-xl">
+            <span className="text-blue-700">OpenCoding</span>
+          </a>
+        </div>
+
+        {/* Auth links in top-right */}
         <div className="absolute top-5 right-5 flex space-x-4">
           <a
             href="#"
-            className="px-1 py-1 text-lg rounded-full hover:text-gray-500"
+            className="px-5 py-1 text-lg border rounded-full hover:text-gray-500"
           >
             Login
           </a>
           <a
             href="#"
-            className="px-6 py-1 text-lg text-white bg-blue-700 border rounded-full hover:bg-blue-500"
-
+            className="px-5 py-1 text-lg border rounded-full hover:text-gray-500"
           >
             Sign Up
           </a>
